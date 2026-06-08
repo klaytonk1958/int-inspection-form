e1409402fbfe17ac7cf8e0fbd409e3d7f8e99771
Vercel <vercel[bot]@users.noreply.github.com>
2026-06-08 17:29:04 +0000

    Install and configure Vercel Speed Insights
    This is an automated pull request generated by Vercel to help install [Speed Insights](https://vercel.com/docs/speed-insights) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 
@@ -26,6 +27,7 @@ export default function RootLayout({
         className={`antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
